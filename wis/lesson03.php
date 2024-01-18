93df1873d462985faa7d2b057c594645cbe04822
RyddSynt <122501041+RyddSynt@users.noreply.github.com>
2024-01-18 21:36:45 +0800

    Add files via upload

---
+++ wis/lesson03.php
@@ -0,0 +1,24 @@
+<?php
+
+/* Multi line comment
+testing 
+for lesson 3 SYSNTAXs
+*/
+echo  "Welcome to Php<br>";
+ $capital = 58;
+ $Capital = 50;
+ print("Variable capital is $capital<br>");
+ print("Variable CaPiTal is $Capital<br>");
+ 
+ $greetings = "Welcome to Php!";
+ 
+ if (3 == 2 + 1)
+ print("Good - I haven't totally lost my mind.<br>");
+
+ if (3 == 2 + 1)
+{
+ print("Good - I haven't totally");
+ print("lost my mind.<br>");
+}
+
+ ?>
---
+++ wis/lesson04.php
@@ -0,0 +1,25 @@
+<?php
+//String
+$name = "Rosen";
+echo $name." is still learning<br>";
+//Integers
+$int_var = 12345;
+$another_int = -12345 + 12345;
+echo $another_int."  is the answer<br>";
+//Doubles
+$many = 2.2888800;
+$many_2 = 2.2111200;
+$few = $many + $many_2;
+print("$many + $many_2 = $few<br>");
+//Boolean
+if (TRUE)
+ print("This will always print<br>");
+else
+ print("This will never print<br>");
+ //null
+ $my_var = NULL;
+ //array
+ $cars = array("Tokyo","Helsinki","Rio");
+ echo $cars['0'];
+
+?>
